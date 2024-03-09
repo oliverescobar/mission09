573a4d0e257e1afcdedcc4a21cc3ec714b610728
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2024-03-08 23:50:52 -0700

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+const basketballTeamNames = [
+  {
+    teamName: '',
+  },
+];
+
+function Welcome() {
+  return <h1>College Basketball Teams!</h1>;
+}
+
+class BBall extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot Name: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function BasketballTeamList() {
+  return (
+    <div>
+      {basketballTeamNames.map((oneTeam) => (
+        <BBall {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Oliver was here.
-        </a>
-      </header>
+      <Welcome />
+      <BasketballTeamList />
     </div>
   );
 }
